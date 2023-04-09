d5f3c44c1bd2e89b9a288aca89f6a6f6ade14a81
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-09 20:21:39 +0530

    chore: connected to mongodb

--- server/index.js
+++ server/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/codeSnippetManager")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to Mongodb...", err));
