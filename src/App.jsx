750c9e5e3ad0a2de6c31321f38a10679582e9d96
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-09 20:15:01 +0530

    chore: created basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navigation from "../components/Navigation";
+import Files from "../components/Files";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-     Set Up
-    </div>
-  )
+    <BrowserRouter>
+      <div className="container">
+        <div className="nav">
+          <Navigation />
+        </div>
+        <div className="col">
+          <Routes>
+            <Route path="/c" element={<Files />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+} 
+
+export default App;
+
+{
+  /* <Route path="/c++" element={<Cpp />} />
+          <Route path="/javascipt" element={<JavaScipt/>} />
+          <Route path="/python" element={<Python />} /> */
 }
 
-export default App
+// {
+//   "langName" : "C"
+//   "snippets" : [{
+//     title,
+//     content
+//   }],
+// }
