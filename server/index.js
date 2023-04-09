82a9b43543048d6e341a392191d15915c5a0e430
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-09 21:07:11 +0530

    chore: created post request to create new language snippet

--- server/index.js
+++ server/index.js
@@ -9,3 +9,30 @@ mongoose
   .connect("mongodb://0.0.0.0:27017/codeSnippetManager")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to Mongodb...", err));
+
+const languageSchema = new mongoose.Schema({
+  name: String,
+  snippet: [{ title: String, code: String }],
+});
+
+const Language = mongoose.model("Language", languageSchema);
+
+
+
+app.post("/api/languages/add", async (req, res) => {
+  const snippetData = req.body.snippet.map((snippet) => ({
+    title: snippet.title,
+    code: snippet.code,
+  }));
+
+  const language = new Language({
+    name: req.body.name,
+    snippet: snippetData,
+  });
+
+  const result = await language.save();
+
+  return res.json(result);
+});
+
+app.listen(5000, () => console.log("Listening on port 5000"));
