0737d7581c15436e9ca639227717228fc5644e51
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-11 16:47:53 +0530

    chore: linked with content component

--- components/Snippets.jsx
+++ components/Snippets.jsx
@@ -1,27 +1,30 @@
 import { useState, useEffect } from "react";
 import "../src/App.css";
 import axios from "axios";
+import Content from "./Content";
 
 function Snippets() {
   // const [snippets, setSnippets] = useState([]);
-  const [snippets, setSnippets] = useState([{name: '', content:''}]);
+  const [snippets, setSnippets] = useState([{ name: "", content: "" }]);
+  const [currentSnippet, setCurrentSnippet] = useState({
+    name: "",
+    content: "",
+  });
   const [newSnippet, setNewSnippet] = useState("");
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/languages")
       .then((res) => {
-        console.log(res.data)
-        res.data.map((data) => data.snippet.map(data => setSnippets(prevState => [...prevState, { name: data.title, content: data.code }]))); 
-
-        // setSnippets(prevState => [...prevState, ...newSnippets]);
-        // console.log(res.data)
-      //   const arr = res.data.map((data) =>
-      //     data.snippet.map((data) => data.title)
-      //   );
-      //   arr.map((data) =>
-      //     setSnippets((prevLanguages) => [...prevLanguages, ...data])
-      //   );
+        console.log(res.data);
+        res.data.map((data) =>
+          data.snippet.map((data) =>
+            setSnippets((prevState) => [
+              ...prevState,
+              { name: data.title, content: data.code },
+            ])
+          )
+        );
         console.log(snippets);
       })
       .catch((err) => {
@@ -34,18 +37,27 @@ function Snippets() {
     setNewSnippet("");
   };
 
+  const handleClick = (name, content) => {
+    setCurrentSnippet({ name, content });
+  };
+
   return (
     <div>
-      {snippets.map((snippet) => (
-        <div>{snippet.name}</div>
-      ))}
-      <div>
-        <input
-          type="text"
-          value={newSnippet}
-          onChange={(e) => setNewSnippet(e.target.value)}
-        />
-        <button onClick={handleAdd}>+ New File</button>
+      <div className="container">
+        <div>
+          {snippets.map((snippet) => (
+            <div onClick={() => handleClick(snippet.name, snippet.content)}>
+              {snippet.name}
+            </div>
+          ))}
+          <input
+            type="text"
+            value={newSnippet}
+            onChange={(e) => setNewSnippet(e.target.value)}
+          />
+          <button onClick={handleAdd}>+ New File</button>
+        </div>
+        <Content code={currentSnippet.content} />
       </div>
     </div>
   );
