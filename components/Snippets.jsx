4834019ae33d39c5f87270f095ab2509d530d8f8
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-12 23:54:19 +0530

    refactor: removed comments and tested network requests

--- components/Snippets.jsx
+++ components/Snippets.jsx
@@ -4,9 +4,6 @@ import axios from "axios";
 import Content from "./Content";
 
 function Snippets({ id, snippets }) {
-  // const [id, setId] = useState("");
-  // const [snippets, setSnippets] = useState([{ name: "", content: "", id: "" }]);
-  // const [snippets, setSnippets] = useState([{ name: "", content: "", id: "" }]);
   const [currentSnippet, setCurrentSnippet] = useState({
     name: "",
     content: "",
@@ -16,32 +13,7 @@ function Snippets({ id, snippets }) {
     name: "",
     content: "",
   });
-
-  // useEffect(()=>{
-  //   setSnippets(mySnippet)
-  // },[])
-
   console.log(snippets.length, "my snippets:", snippets, "and", id);
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:5000/api/languages")
-  //     .then((res) => {
-  //       res.data.map((data) => setId(data._id));
-  //       console.log(res.data);
-  //       res.data.map((data) =>
-  //         data.snippet.map((data) =>
-  //           setSnippets((prevState) => [
-  //             ...prevState,
-  //             { name: data.title, content: data.code, id: data._id },
-  //           ])
-  //         )
-  //       );
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-  // console.log(id);
 
   const handleClick = (name, content, id) => {
     setCurrentSnippet({ name, content, id });
