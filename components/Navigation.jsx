5420bba0f96174fd73f7dab0b2452f286a734c3f
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-12 23:50:06 +0530

    refactor: calls entire language object to pass into Snippet component

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,27 +1,62 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import "../src/App.css";
-import axios from 'axios';
+import axios from "axios";
+import Snippets from "./Snippets";
 
 function Navigation() {
-  const [languages, setLanguages] = useState(['hmmm']);
-  
+  const [languages, setLanguages] = useState([
+    {
+      _id: "",
+      name: "",
+      snippet: [],
+    },
+  ]);
+
+  const [check, setCheck] = useState();
+
+  const [currentLanguage, setCurrentLanguage] = useState({
+    _id: "",
+    name: "",
+    snippet: [],
+  });
+
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/languages")
       .then((res) => {
-        console.log(res.data.map(data => data.name));
-        setLanguages(prevLanguages => [...prevLanguages, ...res.data.map(data => data.name)]);
+        setLanguages(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  },[]);
+  }, []);
+
+  const handleClick = (language) => {
+    setCurrentLanguage(language);
+    setCheck(true);
+  };
+
+  console.log(currentLanguage);
 
   return (
-    <div>
-        {languages.map(lang => <div>{lang}</div>)}
+    <div className="container nav">
+      <div>
+        {languages.map((language) => (
+          <h2 key={language._id} onClick={() => handleClick(language)}>
+            {language.name}
+          </h2>
+        ))}
+      </div>
+      <div>
+        {check && (
+          <Snippets
+            id={currentLanguage._id}
+            snippets={currentLanguage.snippet}
+          />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
