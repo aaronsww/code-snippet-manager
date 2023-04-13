a4e7d62d7dfb7ba6857d1d5c35876e839cbfcaa5
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-04-13 22:28:19 +0530

    refactor: removed unnecesary routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Navigation from "../components/Navigation";
-import Snippets from "../components/Snippets";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <div className="nav">
-          <Navigation />
-        </div>
-        <div className="col">
-          <Routes>
-            <Route path="/c" element={<Snippets />} />
-          </Routes>
-        </div>
-      </div>
+      <Navigation />
     </BrowserRouter>
   );
-} 
-
-export default App;
-
-{
-  /* <Route path="/c++" element={<Cpp />} />
-          <Route path="/javascipt" element={<JavaScipt/>} />
-          <Route path="/python" element={<Python />} /> */
 }
 
-// {
-//   "langName" : "C"
-//   "snippets" : [{
-//     title,
-//     content
-//   }],
-// }
+export default App;
